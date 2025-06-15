cafd4676626d7ff211435313dbd35aab05b20b35
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-06-15 19:17:45 -0300

    Adicionando criação e autenticação do token

---
+++ src/main/java/atividadeJwt/demo/infra/security/TokenService.java
@@ -0,0 +1,52 @@
+package atividadeJwt.demo.infra.security;
+
+import atividadeJwt.demo.entity.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secreto;
+
+    public String geradorToken (Usuario usuario){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secreto);
+
+            String token = JWT.create()
+                    .withIssuer("atividadeJwt") // possivel erro pode ocorrer aqui "nome da aplicacao"
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(this.geradorExpiracao())
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException ex){
+            throw new RuntimeException("Erro enquanto estava autentiacando");
+        }
+    }
+
+    public String validarToiken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secreto);
+            return JWT.require(algorithm)
+                    .withIssuer("atividadeJwt")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException ex) {
+            return null;
+        }
+    }
+
+    private Instant geradorExpiracao (){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
